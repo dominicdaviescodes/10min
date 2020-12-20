1c47e0b56f31a0ad5a955de2c19b87ad0c645050
dominicdaviescodes <codewithdom@gmail.com>
2020-12-20 15:58:31 +0000

    build simple page, add click function to button

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+
+// function buttonClick() {
+//   btn.addEventListener('click',() => {
+//   alert('You Clicked Me!');
+//   })
+// }
+// buttonClick();
+
+btn.addEventListener('click', () => {
+  alert('You Clicked Me!');
+});
